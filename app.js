c7dcb0f7f5a31649553121ce86998c4723ea1d8f
An-jihyun <jihyun.an123@gmail.com>
2021-07-15 01:03:34 +0900

    feat:리셋버튼 완료, 지우개버튼 생성

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
+const resetBtn = document.getElementById("jsReset");
+const eraserBtn = document.getElementById("jsEraser");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 500;
@@ -20,11 +22,11 @@ ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
-function startPainting() {
+function startPainting(event) {
   painting = true;
 }
 
-function stopPainting() {
+function stopPainting(event) {
   painting = false;
 }
 
@@ -79,7 +81,7 @@ function handleCanvasMenu(event) {
   event.preventDefault();
 }
 
-function handleSaveClick() {
+function handleSaveClick(event) {
   const image = canvas.toDataURL();
   const link = document.createElement("a");
   link.href = image;
@@ -87,9 +89,15 @@ function handleSaveClick() {
   link.click();
 }
 
-function handleResetClick() {}
+function handleResetClick(event) {
+  ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
 
-function hand() {}
+function handleEraserClick(event) {
+  const userX = event.clientX;
+  const userY = event.clientY;
+  ctx.clearRect(userX, userY); //작동하지 않음
+}
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
@@ -98,7 +106,6 @@ if (canvas) {
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
   canvas.addEventListener("contextmenu", handleCanvasMenu);
-  canvas.addEventListener("reset", handleResetClick);
 }
 
 Array.from(colors).forEach((color) => color.addEventListener("click", handleColorClick));
@@ -112,4 +119,10 @@ if (mode) {
 }
 if (saveBtn) {
   saveBtn.addEventListener("click", handleSaveClick);
+}
+if (resetBtn) {
+  resetBtn.addEventListener("click", handleResetClick);
+}
+if (eraserBtn) {
+  eraserBtn.addEventListener("click", handleEraserClick);
 }
